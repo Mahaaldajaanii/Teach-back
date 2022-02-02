d7fee1a8c20f72ff326aa26e870bc076efb273d3
Mahaaldajanii <maha050002@gmail.com>
2022-02-02 23:12:49 +0300

    Initial Commit

---
+++ Teach back/Teach_backApp.swift
@@ -0,0 +1,17 @@
+//
+//  Teach_backApp.swift
+//  Teach back
+//
+//  Created by Maha on 01/07/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct Teach_backApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
